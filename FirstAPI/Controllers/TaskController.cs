2b381e030916daa0e3b04d6e25da89037044e034
Azizullah <azizullah422786@gmail.com>
2025-08-28 13:18:59 +0100

    Add project files.

---
+++ FirstAPI/Controllers/TaskController.cs
@@ -0,0 +1,48 @@
+﻿using FirstAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        static private List<TaskItem> tasks = new List<TaskItem>
+        {
+            new TaskItem
+            {
+                Id = 1,
+                Title = "Breakfast at 8 Am",
+                IsDone = true
+            },
+            new TaskItem
+            {
+                Id = 2,
+                Title = "Submit the assignment",
+                IsDone= true
+            },
+            new TaskItem
+            {
+                Id = 3,
+                Title = "Lunch",
+                IsDone = false
+            }
+        };
+        [HttpGet]
+        public ActionResult<List<TaskItem>> GetTasks()
+        {
+            return Ok(tasks);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<TaskItem> GetTaskItemById(int id)
+        {
+            var task = tasks.FirstOrDefault(x  =>  x.Id == id);
+            if (task == null)
+                return NotFound();
+
+            return Ok(task);
+        }
+
+    }
+}
---
+++ FirstAPI/Models/TaskItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstAPI.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        public String Title { get; set; }
+
+        public bool IsDone { get; set; }
+
+    }
+}
